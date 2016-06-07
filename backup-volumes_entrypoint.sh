542d8ba0349d7aff0534b67fcc9704ba80a582da
RikuXan <philipp.holler93@googlemail.com>
2016-06-07 16:36:27 +0200

    added o switch to resolve timestamp error

--- backup-volumes_entrypoint.sh
+++ backup-volumes_entrypoint.sh
@@ -18,7 +18,7 @@ for directory_name in $(find $BACKUP_SOURCEDIR/* -maxdepth 0 -type d -printf "%f
 	# If the directory is a named volume, i.e. no hexadecimal, 64 characters long folder name
 	if [ -z $(echo $directory_name | grep -E '[0-9a-f]{64}') ]; then
 		# rsync it to the target directory and save permissions to a file next to it
-		rsync -avz --no-o --no-g $BACKUP_SOURCEDIR/$directory_name $BACKUP_TARGETDIR/
+		rsync -avzO --no-o --no-g $BACKUP_SOURCEDIR/$directory_name $BACKUP_TARGETDIR/
 		getfacl -R $BACKUP_SOURCEDIR/$directory_name > $BACKUP_TARGETDIR/$directory_name.meta
 	fi
 done
