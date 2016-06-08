5076bdd2be28ca6d8dadd718af3ab2cfba1710a6
RikuXan <philipp.holler93@googlemail.com>
2016-06-08 16:30:04 +0200

    sshfs -> rsync via ssh

--- backup-volumes_entrypoint.sh
+++ backup-volumes_entrypoint.sh
@@ -1,24 +1,22 @@
 #!/bin/bash
 set -e
 
-if [ -z "$SSHFS_CONNECTION" ]; then
+if [ -z "$BACKUP_TARGETDIR" ]; then
   echo "SSHFS_CONNECTION not set."
   exit 1
 fi
 
 : ${BACKUP_SOURCEDIR=/docker-volumes}
-: ${BACKUP_TARGETDIR=/mnt}
-: ${SSHFS_IDENTITYFILE=rhswiwi5.nas.key}
+: ${SSH_IDENTITYFILE=rhswiwi5.nas.key}
 
 chmod 600 $BACKUP_CONFDIR/$SSHFS_IDENTITYFILE
-sshfs -o StrictHostKeyChecking=no -o IdentityFile=$BACKUP_CONFDIR/$SSHFS_IDENTITYFILE $SSHFS_CONNECTION $BACKUP_TARGETDIR
 
 # For each directory in the volume folder do:
 for directory_name in $(find $BACKUP_SOURCEDIR/* -maxdepth 0 -type d -printf "%f\n"); do
 	# If the directory is a named volume, i.e. no hexadecimal, 64 characters long folder name
 	if [ -z $(echo $directory_name | grep -E '[0-9a-f]{64}') ]; then
 		# rsync it to the target directory and save permissions to a file next to it
-		rsync -avzO --no-o --no-g $BACKUP_SOURCEDIR/$directory_name $BACKUP_TARGETDIR/
+		rsync -avz --no-o --no-g -e "ssh -i $BACKUP_CONFDIR/$SSHFS_IDENTITYFILE -o StrictHostKeyChecking=no" $BACKUP_SOURCEDIR/$directory_name $BACKUP_TARGETDIR/
 		getfacl -R $BACKUP_SOURCEDIR/$directory_name > $BACKUP_TARGETDIR/$directory_name.meta
 	fi
 done
