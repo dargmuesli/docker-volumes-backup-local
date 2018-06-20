e867caf7df3ae702036e46d98560b225d46f1086
Christoph Schreyer <christophschreyer@web.de>
2018-06-20 13:12:15 +0200

    Update backup-volumes_entrypoint.sh
    added logging

--- backup-volumes_entrypoint.sh
+++ backup-volumes_entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+LOG_FILE="/var/log/backup/backup_"$(date | sed -e 's/[.: ]/_/g')
+
 if [ -z "$BACKUP_TARGET" ]; then
   echo "BACKUP_TARGET not set."
   exit 1
@@ -16,7 +18,7 @@ for directory_name in $(find $BACKUP_SOURCEDIR/* -maxdepth 0 -type d -printf "%f
 	# If the directory is a named volume, i.e. no hexadecimal, 64 characters long folder name
 	if [ -z $(echo $directory_name | grep -E '[0-9a-f]{64}') ]; then
 		# rsync it to the target directory and save permissions to a file next to it
-		rsync -avz --no-o --no-g -e "ssh -i $BACKUP_CONFDIR/$SSH_IDENTITYFILE -o StrictHostKeyChecking=no" $BACKUP_SOURCEDIR/$directory_name $BACKUP_TARGET/
+		rsync -avz --no-o --no-g -e "ssh -i $BACKUP_CONFDIR/$SSH_IDENTITYFILE -o StrictHostKeyChecking=no" $BACKUP_SOURCEDIR/$directory_name $BACKUP_TARGET/ 2>>$LOG_FILE
 		getfacl -RPpn $BACKUP_SOURCEDIR/$directory_name > /tmp/$directory_name.meta
 		scp -i $BACKUP_CONFDIR/$SSH_IDENTITYFILE -o StrictHostKeyChecking=no /tmp/$directory_name.meta $BACKUP_TARGET/
 	fi
